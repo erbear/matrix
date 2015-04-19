54f9879e36c9016942764b0bdf69ecac6b1beab1
Bartosz Erbert <erbert.b@gmail.com>
2015-04-19 15:49:14 +0200

    Initial Commit

---
+++ project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  project
+//
+//  Created by Bartek on 19.04.2015.
+//  Copyright (c) 2015 Bartek. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
