12ac0361565c87f57ddca0b64a500d28c9b17dfe
Bartosz Erbert <erbert.b@gmail.com>
2015-04-19 15:53:29 +0200

    init

---
+++ project/Matrix.cpp
@@ -0,0 +1,9 @@
+//
+//  Matrix.cpp
+//  project
+//
+//  Created by Bartek on 19.04.2015.
+//  Copyright (c) 2015 Bartek. All rights reserved.
+//
+
+#include "Matrix.h"
---
+++ project/Matrix.h
@@ -0,0 +1,31 @@
+//
+//  Martix.h
+//  AK2
+//
+//  Created by Bartek on 19.04.2015.
+//  Copyright (c) 2015 Bartek. All rights reserved.
+//
+
+#include <vector>
+
+//Potrzebuje:
+
+//wczytanie/wygenerowanie macierzy
+
+//sprawdzenie czy macierz jest symetryczna
+
+//sprawdzanie czy jest trojkatna
+
+//vektor ktory trzyma macierz
+
+//tablice ktore beda przechowywaly dane
+
+//rozmar column, row
+
+
+
+class Martix
+{
+    int c, r;
+    
+};
